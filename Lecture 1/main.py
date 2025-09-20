30b5da8ba818dd46279a4e524c6a79809e1aa0f0
tumana777 <tumana991@gmail.com>
2025-09-20 13:31:51 +0400

    Add lecture 1

---
+++ Lecture 1/main.py
@@ -0,0 +1,37 @@
+# greeting = "Hello World"
+
+# print(greeting)
+# print()
+# print()
+# print("Hello PP-31")
+
+# print('5 + 6')
+
+# print("Hello PP-31", "Hello World", sep="#")
+
+# print("Nino", end="#")
+# print("Aleks")
+
+# input("Enter your name: ")
+#
+# print("Hello")
+
+# name = input("Enter your name: ")
+#
+# print("Hello", name)
+
+# num1 = input("Enter first number: ")
+# num2 = input("Enter second number: ")
+
+# num1 = 5
+# num2 = 7
+
+# num1 = int(input("Enter first number: "))
+# num2 = int(input("Enter second number: "))
+#
+# print(num1 + num2)
+
+# num1 = float(input("Enter first number: "))
+# num2 = float(input("Enter second number: "))
+#
+# print(num1 + num2)
