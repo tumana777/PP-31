1a1f5b63e25990a45395d9e79cb97e4dd3eda9dd
tumana777 <tumana991@gmail.com>
2025-09-20 23:15:56 +0400

    add lecture 2

---
+++ Lecture 2/main.py
@@ -0,0 +1,170 @@
+# a = 5
+
+# a = a + 7
+# a += 7
+
+# a = a - 7
+# a -= 7
+#
+# a = a * 7
+# a *= 7
+#
+# a = a / 7
+# a /= 7
+#
+# a = a // 7
+# a //= 7
+#
+# a = a ** 7
+# a **= 7
+#
+# a = a % 7
+# a %= 7
+
+# print(a)
+
+# print(5 < 9)
+
+# print(5 > 9)
+
+# print(type(5))
+
+# print(type(5.0))
+
+# print(type("Otar"))
+
+# print(type(5 < 9))
+
+# print(5 < 9 and 8 < 10)
+# print(5 > 9 and 8 < 10)
+
+# print(5 < 9 and 8 < 10 and 12 > 7)
+
+# print(5 > 9 or 8 < 10)
+# print(5 < 9 or 8 < 10)
+# print(5 > 9 or 8 > 10)
+
+# print(True and False)
+# print(True and True)
+# print(False and True)
+# print(False and False)
+
+# print(True or False)
+# print(True or True)
+# print(False or True)
+# print(False or False)
+
+# print(not(True and False))
+# print(not(True and True))
+# print(not(False and True))
+# print(not(False and False))
+#
+# print()
+#
+# print(not True and False)
+# print(not True and True)
+# print(not False and True)
+# print(not False and False)
+
+# a = 6
+# b = 6
+#
+# print(id(a))
+# print(id(b))
+
+# print(a is b)
+
+# x = 8
+# y = 8
+#
+# print(a == b)
+
+# lst1 = [1, 2, 3]
+# lst2 = [1, 2, 3]
+# lst3 = lst1
+#
+# print(id(lst1))
+# print(id(lst2))
+# print(id(lst3))
+#
+# print(lst1 == lst2)
+# print(lst1 is lst2)
+# print(lst1 is lst3)
+
+# print('x' in 'matrix')
+
+# char = input("Enter a character: ")
+# sentence = input("Enter a sentence: ")
+#
+# print(char in sentence)
+
+# print(1 in 521)
+
+# num1 = 4
+# num2 = 253
+#
+# print(str(num1) in str(num2))
+
+
+# a = 6
+# b = 9
+# c = a
+#
+# print(a == b)
+# print(a != b)
+#
+# print(a <= b)
+# print(a >= c)
+
+
+# if 8 < 7:
+#     print("Yes")
+#
+# print("End of program")
+
+
+# name = input("Enter your name: ")
+#
+# if name == "Otar":
+#     print("Hello, Otar!")
+# else:
+#     print("Hello, stranger!")
+#
+# if name == "otar":
+#     print("Hello, Otar!")
+#
+# print("End of program")
+
+# name = input("Enter your name: ")
+#
+# if name == "Otar":
+#     print("Hello, Otar!")
+# elif name == "akaki":
+#     print("Hello, akaki!")
+# elif name == "aleqsi":
+#     print("Hello, aleqsi!")
+# else:
+#     print("Hello, stranger!")
+#
+# print("End of program")
+
+
+# name1 = input("Enter name 1: ")
+# name2 = input("Enter name 2: ")
+#
+# if name2 == "aleqsi":
+#     print("Hello, Aleqsi!")
+# elif name1 == "otar":
+#     print("Hello, Otar!")
+# else:
+#     print("Hello, stranger!")
+
+# name1 = input("Enter name 1: ")
+# name2 = input("Enter name 2: ")
+#
+# if name2 == "aleqsi":
+#     print("Hello, Aleqsi!")
+# if name1 == "otar":
+#     print("Hello, Otar!")
+# else:
+#     print("Hello, stranger!")
