eca21affc9a7f73258f10978c4d1f75cbba4a1e9
tumana777 <tumana991@gmail.com>
2025-09-23 21:50:59 +0400

    add lecture 3

---
+++ Lecture 3/main.py
@@ -0,0 +1,124 @@
+# username = 'admin'
+# password = 'admin123'
+# number = 123
+#
+# user_username = input("Enter username: ")
+
+
+# if user_username == username and user_password == password:
+#     print("You are logged in!")
+# else:
+#     print("Wrong username or password!")
+
+# if user_username == username:
+#     user_password = input("Enter password: ")
+#
+#     if user_password == password:
+#         print("You are logged in!")
+#     else:
+#         print("Wrong password!")
+# else:
+#     print("Wrong username!")
+
+# if user_username == username:
+#     user_password = input("Enter password: ")
+#
+#     if user_password == password:
+#         user_number = int(input("Enter number: "))
+#
+#         if user_number == number:
+#             print("You are logged in!")
+#         else:
+#             print("Wrong number!")
+#     else:
+#         print("Wrong password!")
+# else:
+#     print("Wrong username!")
+
+# Loops
+
+# number = 1
+
+# while number <= 10:
+#     print(number)
+#     number += 1
+# else:
+#     print("End of loop")
+
+# while True:
+#     print(number)
+#     number += 1
+#
+#     if number == 10:
+#         break
+
+# total_sum = 0
+#
+# while True:
+#     number = int(input("Enter a number (or 0 to stop): "))
+#
+#     if number == 0:
+#         break
+#     elif number > 0:
+#         total_sum += number
+#     else:
+#         print("Negative numbers are not allowed!")
+#         continue
+#
+#     print(f"Total sum: {total_sum}")
+#
+# print("End of program")
+
+
+# name = "Otar Tumanishvili"
+#
+# for letter in name:
+#     print(letter)
+
+# for i in range(10):
+#     print(i)
+
+# for _ in range(5):
+#     print("Hello, PP-31!")
+
+# for i in range(4, 10):
+#     print(i)
+
+
+# for i in range(1, 11, 2):
+#     print(i)
+
+# for i in range(1, 11, 3):
+#     print(i)
+
+
+# for i in range(10, 1, -2):
+#     print(i)
+
+# i = 1
+#
+# while i <= 20:
+#     print(i)
+#     i *= 2
+
+# name = "Otar"
+# last_name = "Tumanishvili"
+#
+# print(f"Hello, {name} {last_name}!")
+
+
+# for i in range(1, 6):
+#     for j in range(1, 6):
+#         print(f"i = {i}, j = {j}")
+
+import random
+
+# print(random.randint(1, 10))
+
+# print(random.randrange(1, 10))
+
+# print(random.random())
+
+# from random import randint, randrange, choice
+#
+# print(randint(1, 10))
