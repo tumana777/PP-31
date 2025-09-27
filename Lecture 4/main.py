b2941bf2871540a14bdfdc0f0d8fd003dac5c576
tumana777 <tumana991@gmail.com>
2025-09-27 22:14:26 +0400

    add lecture 4

---
+++ Lecture 4/main.py
@@ -0,0 +1,111 @@
+# num1 = int(input("Enter a number1: "))
+# num2 = int(input("Enter a number2: "))
+# num3 = int(input("Enter a number3: "))
+#
+# # 5 6 4
+#
+# largest = num1
+#
+# if num2 > largest:
+#     largest = num2
+#
+# if num3 > largest:
+#     largest = num3
+#
+# print(f"Largest number is: {largest}")
+
+# txt1 = "I love Python"
+#
+# txt2 = "Python is awesome"
+
+# print(txt1 + txt2)
+# print(txt1 * 5)
+
+# print(txt1[3])
+# print(txt1[12])
+
+# print(len(txt1))
+# print(len(txt2))
+
+# print(txt2[16])
+# print(txt2[-1])
+# print(txt2[-17])
+
+# txt1 = "i love Python"
+#
+# txt2 = "Python is awesome"
+
+# print(txt1[2:5])
+# print(txt1[:8])
+# print(txt1[5:])
+# print(txt1[2:9:2])
+# print(txt1[::2])
+# print(txt2[::-1])
+
+# print(txt1.capitalize())
+# print(txt1.upper())
+# print(txt1.lower())
+
+# name = "Aleqsandre"
+#
+# user_input_name = input("Please input your name: ").lower()
+#
+# if user_input_name == name.lower():
+#     print("Hello " + user_input_name)
+
+# print(txt1.count("o"))
+# print(txt1.index("on"))
+# print(txt1.index("a"))
+# print(txt1.find("a"))
+
+# print(txt1.startswith("i"))
+# print(txt1.endswith("on"))
+
+# print(txt1.split("o"))
+
+# txt = input("Enter a string: ")
+#
+# print(txt.strip())
+
+# txt1 = "i love Python"
+#
+# txt2 = "Python is awesome"
+
+# print(txt2.replace("Python", "Java"))
+
+# txt3 = txt1.replace("Python", "JavaScript")
+#
+# print(txt1)
+# print(txt3)
+
+# txt = "Hello"
+# print(type(txt))
+
+# txt = txt.encode()
+#
+# print(len(txt))
+
+# print(type(txt))
+# print(txt)
+
+# txt = "გამარჯობა"
+# print(type(txt))
+# print(len(txt))
+
+# encoded_txt = txt.encode()
+
+# print(len(txt))
+
+# print(type(txt))
+
+# print(txt)
+
+# decoded_txt = encoded_txt.decode()
+#
+# print(decoded_txt)
+
+
+# print(ord("A"))
+# print(ord("a"))
+
+# print(ord("ჭ"))
