65fe579a365ed309af2dae66338a5a6b37a2bd06
tumana777 <tumana991@gmail.com>
2025-10-07 21:57:07 +0400

    add lecture 7

---
+++ Lecture 7/main.py
@@ -0,0 +1,159 @@
+# tuple0 = ()
+
+# print(type(tuple0))
+
+# tuple1 = (1, 2, 3, 4, 5)
+
+# print(len(tuple1)
+
+# print(tuple1[0])
+
+# lst = ["Hello", 2, 3.4, True, [1, 2, 3], "hi"]
+
+# lst[4][1] = 9
+
+
+# print(lst)
+
+# tuple2 = ("Hello", 2, 3.4, True, [1, 2, 3], "hi")
+
+# tuple2[4] = [1, 9, 3]
+
+# tuple2[4][1] = 9
+#
+# print(tuple2)
+
+# lst = ["Hello", 2, 3.4, True, [1, 2, 3], "hi"]
+#
+# my_tuple = tuple(lst)
+
+
+# tuple2 = ("Hello", 2, 3.4, True, [1, 2, 3], "hi")
+#
+# lst = list(tuple2)
+
+# print(lst)
+
+# lst.append(9)
+#
+# tuple2 = tuple(lst)
+#
+# print(tuple2)
+
+# a, *b, c = 1, 2, 3, 4
+#
+# print(a)
+# print(b)
+# print(c)
+
+# a = 1, 2, 3, 4, 8
+#
+# print(type(a))
+
+# tuple1 = ("hello", )
+#
+# print(type(tuple1))
+
+# t = (5 + 4, )
+#
+# print(type(t))
+#
+# print(t)
+
+
+# txt = ("Hello Otar")
+#
+# print(type(txt))
+
+# tuple1 = (1, 2, 3, 4, 5, 3)
+# tuple2 = ("Hello", 2, 3.4, True, [1, 2, 3], "hi")
+#
+# for i in tuple1:
+#     print(i)
+
+# print(tuple1 + tuple2)
+
+# print(tuple1 * 3)
+
+
+# print(3 in tuple2)
+
+# print(tuple1.count(3))
+
+# print(tuple1.index(3))
+
+# my_dict = {
+#     "name": "Otar",
+#     "last_name": "Aleksandrov",
+#     "age": 25,
+#     "city": "Sofia",
+#     "country": "Bulgaria"
+# }
+#
+# print(my_dict)
+
+# a, b, c = 1, 2, 3
+
+# a, b, c = [1, 2, 3]
+#
+# print(a)
+# print(b)
+# print(c)
+
+# set0 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+# lst = ["Hello", 2, 3.4, True, [1, 2, 3], "hi"]
+
+# lst = [2, 3, 4, 2, 6, 4, 8, 8, 10]
+
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# set0 = set(lst)
+#
+# print(set0)
+
+# print(len(set0))
+
+
+# print(set0[2])
+
+
+# myset = {9, 4, 6, 7, 5, 2, 3.14, (1, 2)}
+
+# myset.add(10)
+
+# myset.remove(9)
+
+# myset.pop()
+
+# print(myset)
+
+# set1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13}
+# set2 = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12}
+
+# print(set1.difference(set2))
+# print(set1 - set2)
+
+# print(set1.intersection(set2))
+# print(set1 & set2)
+
+# print(set1.union(set2))
+# print(set1 | set2)
+
+# print(set1.issubset(set2))
+# print(set1 <= set2)
+
+# print(set1.symmetric_difference(set2))
+# print(set1 ^ set2)
+
+
+
+# my_set = {"hello", 2, 1, 3, 4, 5, 6, 7, 8, 9, 10}
+
+# frozen_set = frozenset(my_set)
+#
+# print(type(frozen_set))
+
+# for i in my_set:
+#     print(i)
+
