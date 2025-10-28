a3d254069515ea2e3664430f1acd43e8d9b1d745
tumana777 <tumana991@gmail.com>
2025-10-28 22:08:17 +0400

    add lecture 13

---
+++ Lecture 13/main.py
@@ -0,0 +1,31 @@
+# def test(file_name):
+#     with open(file_name, 'r') as file:
+#         data = file.read()
+#         # file.seek(0)
+#         # lines = file.readlines()
+#         # lines_count = len(lines)
+#         # words_count = len(data.split())
+#         # longest_line = max(lines, key=len)
+#
+#     lines_count = len(data.splitlines())
+#     words_count = len(data.split())
+#     longest_line = max(data.split('\n'), key=len)
+#
+#     return f"lines count: {lines_count}, words count: {words_count}, longest line: {longest_line}"
+#
+# print(test('test.txt'))
+
+# def test(file_name, word):
+#     with open(file_name, 'r') as file:
+#         data = file.read()
+#
+#     count = data.split().count(word)
+#
+#     return count
+#
+# print(test('test.txt', 'love'))
+
+
+
+
+
