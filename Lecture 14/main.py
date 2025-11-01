de0933b005e1b6d8a03181b249705f7010b56732
tumana777 <tumana991@gmail.com>
2025-11-01 22:24:06 +0400

    add lecture 14

---
+++ Lecture 14/main.py
@@ -0,0 +1,64 @@
+# class Student:
+#     status = True
+#     pay = 1000
+#
+# student1 = Student()
+# student2 = Student()
+# student1.status = False
+
+# print(student1.status)
+# print(student2.status)
+
+# print(type(student1))
+
+# print(student1.pay)
+# print(student2.pay)
+
+# student1.pay = 1200
+#
+# print(student1.pay)
+# print(student2.pay)
+
+# class Student:
+#     status = True
+#     pay = 1000
+#     total_students = 0
+#
+#     def __init__(self, first_name, last_name, age):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.age = age
+#         # self.status = False
+#         Student.total_students += 1
+#
+#     def get_email(self):
+#         return f"{Student.status}.{self.last_name}@gmail.com"
+#
+# student1 = Student('walter', 'white', 55)
+# student2 = Student('jessie', 'pinkman', 30)
+#
+# print(student1.get_email())
+
+# x = Student("dsfv", 'sgdf', 25)
+#
+# print(Student.total_students)
+
+# print(student1.first_name)
+# print(student1.last_name)
+# print(student1.age)
+# print(student1.pay)
+# print(student1.status)
+#
+# print(student2.first_name)
+# print(student2.last_name)
+# print(student2.age)
+# print(student2.pay)
+# print(student2.status)
+
+
+
+
+
+
+
+
